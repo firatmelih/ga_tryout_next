d4e44d30104a1f892df40bfcc7c6737f13adf759
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2023-04-13 15:12:34 +0200

    add pages

---
+++ src/pages/example.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      previewing example pages
+      <Link href={'/'}>back to main</Link>
+    </div>
+  )
+}
